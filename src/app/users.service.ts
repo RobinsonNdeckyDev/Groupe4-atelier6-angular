4e636bfe1d814319f0fd9ab99764e9b4dfe2568f
unknown <noelrobinsonndecky@gmail.com>
2023-11-21 13:32:34 +0100

    mdf Robin

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UsersService {
+export class ApiService {
+  
+  private baseUrl = 'https://jsonplaceholder.typicode.com/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+  return this.http.get<any[]>(`${this.baseUrl}users`);
+}
+
 }
--- src/app/utilisateurs/utilisateurs.component.ts
+++ src/app/utilisateurs/utilisateurs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { ApiService } from '../users.service';
 @Component({
   selector: 'app-utilisateurs',
   templateUrl: './utilisateurs.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class UtilisateursComponent {
 
+ users: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 }
